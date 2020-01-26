1c8cb3febf64b44958f47481d2e82192164362a1
jakubk97 <jkuba-kar@o2.pl>
2020-01-26 17:40:39 +0100

    Update RIPEMD160Lib.cs

--- Csharp/Csharp/RIPEMD160_HASH/RIPEMD160Lib.cs
+++ Csharp/Csharp/RIPEMD160_HASH/RIPEMD160Lib.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Security.Cryptography;
 using System.IO;
 
@@ -9,32 +9,41 @@ namespace HashProgram.RIPEMD160_HASH
 
         public RIPEMD160Lib(string fileName)
         {
+
+        }
+
+        public string Compute(string fileName)
+        {
             string directory = fileName;
             try
             {
+
                 RIPEMD160 myRIPEMD160 = RIPEMD160.Create();
                 byte[] hashValue;
                 hashValue = myRIPEMD160.ComputeHash(File.ReadAllBytes(fileName));
-                Console.Write("The RIPEMD160 LIB hash is: ");
-                PrintByteArray(hashValue);
+                return PrintByteArray(hashValue);
             }
             catch (DirectoryNotFoundException)
             {
                 Console.WriteLine("Error: The directory specified could not be found.");
+                return "";
             }
             catch (IOException)
             {
                 Console.WriteLine("Error: A file in the directory could not be accessed.");
+                return "";
             }
+            return "";
         }
 
-        private static void PrintByteArray(byte[] array)
+        private string PrintByteArray(byte[] array)
         {
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(String.Format("{0:X2}", array[i]).ToUpper());
-            }
-            Console.WriteLine();
+                string hash = "";
+                for (int i = 0; i < array.Length; i++)
+                {
+                hash += String.Format("{0:X2}", array[i]).ToUpper();
+                }
+            return hash;
         }
 
 
